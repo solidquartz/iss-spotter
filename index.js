8042175daae1d977c40d9a18538ea983b9195134
Aky Mac Donald <kotoko@Kotoko.local>
2022-06-09 22:40:40 -0400

    add findGeoByIP

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('142.114.231.93', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:', coordinates);
+// });
--- iss.js
+++ iss.js
@@ -1,26 +1,70 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const request = require('request');
 
+// Makes a single API request to retrieve the user's IP address.
+// Input:
+// - A callback (to pass back an error or the IP string)
+// Returns (via Callback):
+// - An error, if any (nullable)
+// - The IP address as a string (null if error)
+
+
 const fetchMyIP = function(callback) {
-  request(`https://api.ipify.org?format=json`, function(error, response, body) {
+  request(`https://api.ipify.org?format=json`, (error, response, body) => {
 
-    const data = JSON.parse(body);
-
+    // error can be set if invalid domain, user is offline, etc.
     if (error) {
-      return callback(error);
+      return callback(error, null);
     }
 
-    return callback(null, data.ip);
-    
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = JSON.parse(body);
+    return callback(null, ip.ip);
+
   });
 
 };
 
-module.exports = { fetchMyIP };
+
+
+
+////////////////////////////////////////////
+
+//Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+//Input:
+//- The ip (ipv4) address (string)
+//- A callback (to pass back an error or the lat/lng object)
+//Returns (via Callback):
+//- An error, if any (nullable)
+//- The lat and lng as an object (null if error).
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://api.ipbase.com/v2/info?apikey=2JLtDwYebipZyqSKsActC0u0rkhoRxEhpax7NSWL&ip=${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const geoInfo = JSON.parse(body);
+    const coords = {
+      latitude: geoInfo.data.location.latitude,
+      longitude: geoInfo.data.location.longitude
+    };
+    
+    callback(null, coords);
+
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
